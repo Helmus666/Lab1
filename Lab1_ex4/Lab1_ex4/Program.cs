16f41498dd62c51a92779641599923009f38b0ba
Helmus666 <helmus228@gmail.com>
2025-03-20 00:27:49 +0200

    Add files via upload

---
+++ Lab1_ex4/Lab1_ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Виконав: [Андрущенко Роман]");
+
+        Console.Write("Введіть кількість комп'ютерів у класі №1: ");
+        int C1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть кількість комп'ютерів у класі №2: ");
+        int C2 = Convert.ToInt32(Console.ReadLine());
+
+        C1 += 1; 
+        C2 += 2; 
+
+        Console.WriteLine($"Оновлена кількість комп'ютерів у класі №1: {C1}");
+        Console.WriteLine($"Оновлена кількість комп'ютерів у класі №2: {C2}");
+    }
+}
