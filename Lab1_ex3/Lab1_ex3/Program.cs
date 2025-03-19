6351c9832f87aefb388badcb60ad7bffc6052d00
Helmus666 <helmus228@gmail.com>
2025-03-20 00:20:44 +0200

    Add files via upload

---
+++ Lab1_ex3/Lab1_ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Виведення імені та прізвища студента
+        Console.WriteLine("Виконав: [Андрущенко Роман]");
+
+        Console.Write("Введіть ціну булочки (грн): ");
+        double X = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть кількість булочок: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        double totalPrice = X * N;
+        int hryvnias = (int)totalPrice;
+        int kopecks = (int)Math.Round((totalPrice - hryvnias) * 100);
+
+        Console.WriteLine($"Сума до сплати: {hryvnias} гривень {kopecks} копійок");
+    }
+}
