b3fd4958e43898f5f6b5a59848101c5161cbc06b
Helmus666 <helmus228@gmail.com>
2025-03-20 00:14:31 +0200

    Add files via upload

---
+++ Lab1_ex2/Lab1_ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Виконав: [Андрущенко Роман]");
+
+        Console.Write("Хвилин: ");
+        int totalMinutes = Convert.ToInt32(Console.ReadLine());
+
+        int hours = totalMinutes / 60;
+        int minutes = totalMinutes % 60;
+
+        Console.WriteLine($"{hours}:{minutes}");
+    }
+}
