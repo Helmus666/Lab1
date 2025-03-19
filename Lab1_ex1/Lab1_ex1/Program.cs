a0c9443b0ed0e130c564ea63dda5679b15457630
Helmus666 <helmus228@gmail.com>
2025-03-19 23:58:40 +0200

    Add files via upload

---
+++ Lab1_ex1/Lab1_ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Виконав: [Андрущенко Роман]");
+
+        double x = 12.5;
+
+        Console.Write("Значення a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        double y = 8.5 * Math.Pow(x, (9 + 3 * a) / (a + 1));
+
+        Console.WriteLine($"x = {x}");
+        Console.WriteLine($"a = {a}");
+        Console.WriteLine($"y = {y}");
+        Console.WriteLine($"√y = {Math.Sqrt(y)}");
+        Console.WriteLine($"x^4 = {Math.Pow(x, 4)}");
+    }
+}
